efc4cd93303aff894a027fd2b739462e5fadc883
EveMvl <evelinemarvelia@gmail.com>
2025-11-30 23:50:50 +0700

    Upload Android Studio project

---
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lab_week_10
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: TotalViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[TotalViewModel::class.java]
+
+        val textTotal = findViewById<TextView>(R.id.text_total)
+        val buttonIncrement = findViewById<Button>(R.id.button_increment)
+
+        viewModel.total.observe(this, Observer { total ->
+            textTotal.text = getString(R.string.text_total, total)
+        })
+
+        buttonIncrement.setOnClickListener {
+            viewModel.incrementTotal()
+        }
+    }
+}
